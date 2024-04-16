21281d6eedb4ef1f229192a8719a6b0b893178bf
dagon0721 <153082512+dagon0721@users.noreply.github.com>
2024-04-16 23:48:30 +0900

    Update CBY_Database.sql
    sql

--- app/Back/CBY_Database.sql
+++ app/Back/CBY_Database.sql
@@ -1,34 +1,36 @@
-CREATE TABLE `User` (
-  `UserID` VARCHAR(255) PRIMARY KEY,
-  `Name` VARCHAR(255),
-  `Password` VARCHAR(255),
-  `RegisterationDate` TIMESTAMP
+CREATE TABLE member_table(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    memberEmail VARCHAR(255) UNIQUE,
+    memberPassword VARCHAR(255),
+    memberName VARCHAR(255),
+    signDate TIMESTAMP,
+    memberBirthDate VARCHAR(255)
 );
 
-CREATE TABLE `Question` (
-  `QuestionID` INTEGER PRIMARY KEY,
-  `Content` VARCHAR(255)
+CREATE TABLE Question (
+    QuestionID INTEGER PRIMARY KEY,
+    Content VARCHAR(255)
 );
 
-CREATE TABLE `Answer` (
-  `AnswerID` INTEGER PRIMARY KEY,
-  `UserID` VARCHAR(255),
-  `QuestionID` INTEGER,
-  `Content` VARCHAR(255),
-  `CreationDate` TIMESTAMP,
-  FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`),
-  FOREIGN KEY (`QuestionID`) REFERENCES `Question` (`QuestionID`)
+CREATE TABLE Answer (
+    AnswerID INTEGER PRIMARY KEY,
+    member_id BIGINT,
+    QuestionID INTEGER,
+    Content VARCHAR(255),
+    CreationDate TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member_table(id),
+    FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
 );
 
-CREATE TABLE `Image` (
-  `ImageID` INTEGER PRIMARY KEY,
-  `ImagePath` VARCHAR(255)
+CREATE TABLE Image (
+    ImageID INTEGER PRIMARY KEY,
+    ImagePath VARCHAR(255)
 );
 
-CREATE TABLE `Diary` (
-  `DiaryID` INTEGER PRIMARY KEY,
-  `UserID` VARCHAR(255),
-  `Content` VARCHAR(255),
-  `CreationDate` TIMESTAMP,
-  FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`)
+CREATE TABLE Diary (
+    DiaryID INTEGER PRIMARY KEY,
+    member_id BIGINT,
+    Content VARCHAR(255),
+    CreationDate TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member_table(id)
 );
